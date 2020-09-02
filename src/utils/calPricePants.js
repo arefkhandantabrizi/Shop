937245d53d22dd72a8da13c0027083f4746a9f31
arefkhandantabrizi <aref.khandantabrizi@gmail.com>
2020-09-03 00:42:57 +0430

    Updating price

--- src/utils/calPricePants.js
+++ src/utils/calPricePants.js
@@ -2,78 +2,78 @@ export default function calPricePants(Height, userSchool) {
   let height = parseFloat(Height);
   switch (userSchool) {
     case "دبستان پسرانه شهاب":
-      if (height >= 0 && height <= 75) return 32000;
-      if (height > 75 && height <= 85) return 34000;
-      if (height > 85 && height <= 105) return 36000;
-      if (height > 105 && height <= 120) return 37000;
+      if (height >= 0 && height <= 75) return 32500;
+      if (height > 75 && height <= 85) return 34500;
+      if (height > 85 && height <= 105) return 36500;
+      if (height > 105 && height <= 120) return 37500;
       break;
     case "دبستان پسرانه فرهنگ":
-      if (height >= 0 && height <= 75) return 32000;
-      if (height > 75 && height <= 85) return 34000;
-      if (height > 85 && height <= 105) return 36000;
-      if (height > 105 && height <= 120) return 37000;
+      if (height >= 0 && height <= 75) return 32500;
+      if (height > 75 && height <= 85) return 34500;
+      if (height > 85 && height <= 105) return 36500;
+      if (height > 105 && height <= 120) return 37500;
       break;
     case "دبستان پسرانه صبا":
-      if (height >= 0 && height <= 75) return 34000;
-      if (height > 75 && height <= 85) return 36000;
-      if (height > 85 && height <= 105) return 38000;
-      if (height > 105 && height <= 120) return 39000;
+      if (height >= 0 && height <= 75) return 34500;
+      if (height > 75 && height <= 85) return 36500;
+      if (height > 85 && height <= 105) return 38500;
+      if (height > 105 && height <= 120) return 39500;
       break;
     case "دبستان پسرانه گوهران":
-      if (height >= 0 && height <= 75) return 34000;
-      if (height > 75 && height <= 85) return 36000;
-      if (height > 85 && height <= 105) return 38000;
-      if (height > 105 && height <= 120) return 39000;
+      if (height >= 0 && height <= 75) return 34500;
+      if (height > 75 && height <= 85) return 36500;
+      if (height > 85 && height <= 105) return 38500;
+      if (height > 105 && height <= 120) return 39500;
       break;
     case "دبستان پسرانه امام حسین":
-      if (height >= 0 && height <= 75) return 34000;
-      if (height > 75 && height <= 85) return 36000;
-      if (height > 85 && height <= 105) return 38000;
-      if (height > 105 && height <= 120) return 39000;
+      if (height >= 0 && height <= 75) return 34500;
+      if (height > 75 && height <= 85) return 36500;
+      if (height > 85 && height <= 105) return 38500;
+      if (height > 105 && height <= 120) return 39500;
       break;
     case "دبستان پسرانه فضیلت":
-      if (height >= 0 && height <= 75) return 32000;
-      if (height > 75 && height <= 85) return 34000;
-      if (height > 85 && height <= 105) return 36000;
-      if (height > 105 && height <= 120) return 61500;
+      if (height >= 0 && height <= 75) return 32500;
+      if (height > 75 && height <= 85) return 34500;
+      if (height > 85 && height <= 105) return 36500;
+      if (height > 105 && height <= 120) return 37500;
       break;
     case "دبستان دخترانه فرهنگ":
-      if (height >= 0 && height <= 75) return 32000;
-      if (height > 75 && height <= 85) return 34000;
-      if (height > 85 && height <= 105) return 36000;
-      if (height > 105 && height <= 120) return 37000;
+      if (height >= 0 && height <= 75) return 32500;
+      if (height > 75 && height <= 85) return 34500;
+      if (height > 85 && height <= 105) return 36500;
+      if (height > 105 && height <= 120) return 37500;
       break;
 
     case "دبستان دخترانه مرحمت صائب":
-      if (height >= 0 && height <= 75) return 32000;
-      if (height > 75 && height <= 85) return 34000;
-      if (height > 85 && height <= 105) return 36000;
-      if (height > 105 && height <= 120) return 37000;
+      if (height >= 0 && height <= 75) return 32500;
+      if (height > 75 && height <= 85) return 34500;
+      if (height > 85 && height <= 105) return 36500;
+      if (height > 105 && height <= 120) return 37500;
       break;
 
     case "دبستان دخترانه فضیلت":
-      if (height >= 0 && height <= 75) return 34000;
-      if (height > 75 && height <= 85) return 36000;
-      if (height > 85 && height <= 105) return 38000;
-      if (height > 105 && height <= 120) return 39000;
+      if (height >= 0 && height <= 75) return 34500;
+      if (height > 75 && height <= 85) return 36500;
+      if (height > 85 && height <= 105) return 38500;
+      if (height > 105 && height <= 120) return 39500;
       break;
 
     case "متوسطه دوره دوم دخترانه رازی":
-      if (height >= 0 && height <= 105) return 40000;
-      if (height > 105 && height <= 110) return 45000;
-      if (height > 110 && height <= 120) return 50000;
+      if (height >= 0 && height <= 105) return 40500;
+      if (height > 105 && height <= 110) return 45500;
+      if (height > 110 && height <= 120) return 50500;
       break;
 
     case "متوسطه دوره دوم دخترانه الزهرا":
-      if (height >= 0 && height <= 105) return 40000;
-      if (height > 105 && height <= 110) return 45000;
-      if (height > 110 && height <= 120) return 50000;
+      if (height >= 0 && height <= 105) return 40500;
+      if (height > 105 && height <= 110) return 45500;
+      if (height > 110 && height <= 120) return 50500;
       break;
 
     case "متوسطه دوره دوم دخترانه تیزهوشان":
-      if (height >= 0 && height <= 105) return 40000;
-      if (height > 105 && height <= 110) return 45000;
-      if (height > 110 && height <= 120) return 50000;
+      if (height >= 0 && height <= 105) return 40500;
+      if (height > 105 && height <= 110) return 45500;
+      if (height > 110 && height <= 120) return 50500;
       break;
 
     case "متوسطه دوره اول دخترانه نمونه اندیشه":
--- src/utils/calPriceShirtAndJacket.js
+++ src/utils/calPriceShirtAndJacket.js
@@ -2,81 +2,81 @@ export default function calPriceShirtAndJacket(Height, userSchool) {
   let height = parseFloat(Height);
   switch (userSchool) {
     case "دبستان پسرانه شهاب":
-      if (height >= 0 && height <= 58) return 50500;
-      if (height > 58 && height <= 65) return 54000;
-      if (height > 65 && height <= 73) return 60500;
-      if (height > 73 && height <= 75) return 61500;
+      if (height >= 0 && height <= 58) return 51500;
+      if (height > 58 && height <= 65) return 55000;
+      if (height > 65 && height <= 73) return 61500;
+      if (height > 73 && height <= 75) return 62500;
       break;
     case "دبستان پسرانه فرهنگ":
-      if (height >= 0 && height <= 58) return 50500;
-      if (height > 58 && height <= 65) return 54000;
-      if (height > 65 && height <= 73) return 60500;
-      if (height > 73 && height <= 75) return 61500;
+      if (height >= 0 && height <= 58) return 51500;
+      if (height > 58 && height <= 65) return 55000;
+      if (height > 65 && height <= 73) return 61500;
+      if (height > 73 && height <= 75) return 62500;
       break;
     case "دبستان پسرانه صبا":
-      if (height >= 0 && height <= 58) return 54000;
-      if (height > 58 && height <= 65) return 57500;
-      if (height > 65 && height <= 73) return 62000;
-      if (height > 73 && height <= 75) return 63000;
+      if (height >= 0 && height <= 58) return 55000;
+      if (height > 58 && height <= 65) return 58500;
+      if (height > 65 && height <= 73) return 63000;
+      if (height > 73 && height <= 75) return 64000;
       break;
     case "دبستان پسرانه گوهران":
-      if (height >= 0 && height <= 58) return 54000;
-      if (height > 58 && height <= 65) return 57500;
-      if (height > 65 && height <= 73) return 62000;
-      if (height > 73 && height <= 75) return 63000;
+      if (height >= 0 && height <= 58) return 55000;
+      if (height > 58 && height <= 65) return 58500;
+      if (height > 65 && height <= 73) return 63000;
+      if (height > 73 && height <= 75) return 64000;
       break;
     case "دبستان پسرانه امام حسین":
-      if (height >= 0 && height <= 58) return 59500;
-      if (height > 58 && height <= 65) return 65500;
-      if (height > 65 && height <= 73) return 74000;
-      if (height > 73 && height <= 75) return 75000;
+      if (height >= 0 && height <= 58) return 60500;
+      if (height > 58 && height <= 65) return 66500;
+      if (height > 65 && height <= 73) return 75000;
+      if (height > 73 && height <= 75) return 76000;
       break;
     case "دبستان پسرانه فضیلت":
-      if (height >= 0 && height <= 58) return 52000;
-      if (height > 58 && height <= 65) return 55500;
-      if (height > 65 && height <= 73) return 60000;
-      if (height > 73 && height <= 75) return 61000;
+      if (height >= 0 && height <= 58) return 53000;
+      if (height > 58 && height <= 65) return 56500;
+      if (height > 65 && height <= 73) return 61000;
+      if (height > 73 && height <= 75) return 62000;
       break;
     case "دبستان دخترانه فرهنگ":
-      if (height >= 0 && height <= 75) return 52500;
-      if (height > 75 && height <= 85) return 56000;
-      if (height > 85 && height <= 95) return 59500;
-      if (height > 95 && height <= 120) return 60500;
+      if (height >= 0 && height <= 75) return 53500;
+      if (height > 75 && height <= 85) return 57000;
+      if (height > 85 && height <= 95) return 60500;
+      if (height > 95 && height <= 120) return 61500;
       break;
 
     case "دبستان دخترانه مرحمت صائب":
-      if (height >= 0 && height <= 75) return 52500;
-      if (height > 75 && height <= 85) return 56000;
-      if (height > 85 && height <= 95) return 59500;
-      if (height > 95 && height <= 120) return 60500;
+      if (height >= 0 && height <= 75) return 53500;
+      if (height > 75 && height <= 85) return 57000;
+      if (height > 85 && height <= 95) return 60500;
+      if (height > 95 && height <= 120) return 61500;
       break;
 
     case "دبستان دخترانه فضیلت":
-      if (height >= 0 && height <= 75) return 53000;
-      if (height > 75 && height <= 85) return 56500;
-      if (height > 85 && height <= 95) return 60000;
-      if (height > 95 && height <= 120) return 61000;
+      if (height >= 0 && height <= 75) return 54000;
+      if (height > 75 && height <= 85) return 57500;
+      if (height > 85 && height <= 95) return 61000;
+      if (height > 95 && height <= 120) return 62000;
       break;
 
     case "متوسطه دوره دوم دخترانه رازی":
-      if (height >= 0 && height <= 101) return 70000;
-      if (height > 101 && height <= 108) return 76000;
-      if (height > 108 && height <= 115) return 82000;
-      if (height > 115 && height <= 120) return 83000;
+      if (height >= 0 && height <= 101) return 71000;
+      if (height > 101 && height <= 108) return 77000;
+      if (height > 108 && height <= 115) return 83000;
+      if (height > 115 && height <= 120) return 84000;
       break;
 
     case "متوسطه دوره دوم دخترانه الزهرا":
-      if (height >= 0 && height <= 101) return 70000;
-      if (height > 101 && height <= 108) return 76000;
-      if (height > 108 && height <= 115) return 82000;
-      if (height > 115 && height <= 120) return 83000;
+      if (height >= 0 && height <= 101) return 71000;
+      if (height > 101 && height <= 108) return 77000;
+      if (height > 108 && height <= 115) return 83000;
+      if (height > 115 && height <= 120) return 84000;
       break;
 
     case "متوسطه دوره دوم دخترانه تیزهوشان":
-      if (height >= 0 && height <= 101) return 70000;
-      if (height > 101 && height <= 108) return 76000;
-      if (height > 108 && height <= 115) return 82000;
-      if (height > 115 && height <= 120) return 83000;
+      if (height >= 0 && height <= 101) return 71000;
+      if (height > 101 && height <= 108) return 77000;
+      if (height > 108 && height <= 115) return 83000;
+      if (height > 115 && height <= 120) return 84000;
       break;
 
     case "متوسطه دوره اول دخترانه نمونه اندیشه":
